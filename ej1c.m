407a4782f338c19fca823b3c90ed7173a2bbbb80
SebastianIRodriguez <seba.rodriguez.994@gmail.com>
2022-05-26 09:53:04 -0300

    integracion numerica corregida

--- ej1c.m
+++ ej1c.m
@@ -10,8 +10,8 @@ x1(1)=0;
 x2(1)=0;
 
 %Pasos de integracion
-T1 = 2*pi*sqrt(L*C) / (100 * sqrt( 1-C*R^2 / (4*L) ) );
-T2 = 2*pi*sqrt(L*C) / (15 * sqrt( 1-C*R^2 / (4*L) ) );
+T1 = 2*pi*sqrt(L*C) / (100 * sqrt( 1 - (C*(R^2) / (4*L)) ) );
+T2 = 2*pi*sqrt(L*C) / (15 * sqrt( 1 - (C*(R^2) / (4*L)) ) );
 T=T2;
 
 %Definicion de Entrada
@@ -19,9 +19,9 @@ anchoPulso = round(3e-3/T);
 u = [ones(anchoPulso,1); zeros(anchoPulso,1)];
 
 %Integracion numerica
-for n=2:1:length(u)   
+for n=2:1:length(u)
     x1(n) = x2(n-1)*T + x1(n-1);
-    x2(n) = x2(n-1) + (u(n) - R*x2(n-1) - x1(n)/C )*T/L;
+    x2(n) = x2(n-1) + (u(n-1) - R*x2(n-1) - x1(n-1)/C )*T/L;
 end
 
 %Calculo expresiones pedidas
@@ -29,7 +29,7 @@ uc = x1 / C;
 il = x2;
 
 %Graficas
-abcisas= 0:T:(6e-3)-T;
+abcisas = 0:T:(6e-3)-T;
 
 subplot(2,1,1), plot(abcisas, uc);
 title('Tension del Capacitor');
